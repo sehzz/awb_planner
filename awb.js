11f08f48ebc96231b0cb3dad03c524a9502995f4
Sehzz <sehajjot20@gmai.com>
2025-02-18 21:33:05 +0100

    first commit

--- awb.js
+++ awb.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
             const bins = document.querySelectorAll(".bin img");
 
             const binImages = {
-                "blau": "/awb/images/Papiertonnen.jpg",
-                "braun": "/awb/images/Biotonne.jpg",
-                "grau": "/awb/images/Restmuell.jpg",
-                "gelb": "/awb/images/gelbtonne.jpg"
+                "blau": "images/Papiertonnen.jpg",
+                "braun": "images/Biotonne.jpg",
+                "grau": "images/Restmuell.jpg",
+                "gelb": "images/gelbtonne.jpg"
             };
 
             const todaysEntry = garbageData.find(entry => entry.start === today);
